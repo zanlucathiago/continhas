6ed1801981d0224669421eb7edbf08fd750ee5bf
Thiago Zanluca <thiago.zanluca@contaazul.com>
2022-06-20 19:39:02 -0300

    feat: async handler

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getTransactions = (req, res) => {
+const getTransactions = asyncHandler(async (req, res) => {
   res.status(200).json('goals')
-}
+})
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-const setTransaction = (req, res) => {
+const setTransaction = asyncHandler(async (req, res) => {
   if (!req.body.date) {
     res.status(400)
     throw new Error('Adicione uma data para a transação');
   }
 
   res.status(200).json('goal')
-}
+})
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateTransaction = (req, res) => {
+const updateTransaction = asyncHandler(async (req, res) => {
   res.status(200).json('updatedTransaction')
-}
+})
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteTransaction = (req, res) => {
+const deleteTransaction = asyncHandler(async (req, res) => {
   res.status(200).json({ id: req.params.id })
-}
+})
 
 module.exports = {
   getTransactions,
