cb0dde668d512937097dfc1426201557212df5ae
Thiago Zanluca <thiago.zanluca@contaazul.com>
2022-06-20 20:38:02 -0300

    feat: connecting controller to db

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,35 +1,61 @@
 const asyncHandler = require('express-async-handler')
 
-// @desc    Get goals
-// @route   GET /api/goals
+const Transaction = require('../models/transactionModel')
+
+// @desc    Get transactions
+// @route   GET /api/transactions
 // @access  Private
 const getTransactions = asyncHandler(async (req, res) => {
-  res.status(200).json('goals')
+  const transactions = await Transaction.find()
+  res.status(200).json(transactions)
 })
 
-// @desc    Set goal
-// @route   POST /api/goals
+// @desc    Set transaction
+// @route   POST /api/transactions
 // @access  Private
 const setTransaction = asyncHandler(async (req, res) => {
   if (!req.body.description) {
     res.status(400)
     throw new Error('Adicione uma descrição para a transação');
   }
+  const transaction = await Transaction.create({
+    description: req.body.description,
+  })
 
-  res.status(200).json('goal')
+  res.status(200).json(transaction)
 })
 
-// @desc    Update goal
-// @route   PUT /api/goals/:id
+// @desc    Update transaction
+// @route   PUT /api/transactions/:id
 // @access  Private
 const updateTransaction = asyncHandler(async (req, res) => {
-  res.status(200).json('updatedTransaction')
+  const transaction = await Transaction.findById(req.params.id)
+
+  if (!transaction) {
+    res.status(400)
+    throw new Error('Transação não encontrada')
+  }
+
+  const updatedTransaction = await Transaction.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+
+  res.status(200).json(updatedTransaction)
 })
 
-// @desc    Delete goal
-// @route   DELETE /api/goals/:id
+// @desc    Delete transaction
+// @route   DELETE /api/transactions/:id
 // @access  Private
 const deleteTransaction = asyncHandler(async (req, res) => {
+  const transaction = await Transaction.findById(req.params.id)
+
+  if (!transaction) {
+    res.status(400)
+    throw new Error('Transação não encontrada')
+  }
+
+  await transaction.remove()
+
   res.status(200).json({ id: req.params.id })
 })
 
