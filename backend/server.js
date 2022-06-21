30eaacf9cc52a73e13f11322b87eedeef474e656
Thiago Zanluca <thiago.zanluca@contaazul.com>
2022-06-21 11:00:40 -0300

    feat: post user API

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/transactions', require('./routes/transactionRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
