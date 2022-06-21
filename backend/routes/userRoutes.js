b8844a05abdbefed61b54200c494e52ab5a95203
Thiago Zanluca <thiago.zanluca@contaazul.com>
2022-06-21 11:08:01 -0300

    feat: added bcrypt, userController definition

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const {
   registerUser,
-  // loginUser,
-  // getMe,
+  loginUser,
+  getMe,
 } = require('../controllers/userController')
 // const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
-// router.post('/login', loginUser)
+router.post('/login', loginUser)
 // router.get('/me', protect, getMe)
 
 module.exports = router
