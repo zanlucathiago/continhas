9d1b8c535eab6eb868a98722dbe36a0a91b6227b
Thiago Zanluca <thiago.zanluca@contaazul.com>
2022-06-22 12:04:22 -0300

    feat: Router and material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import Container from '@mui/material/Container';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
-    <div>
-      Continhas
-    </div>
-  );
+    <Router>
+      <Container maxWidth="sm">
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Container>
+    </Router>
+  )
 }
 
-export default App;
+export default App
